8197cfcc944bd0f77e66b670772e7bcd80c5ad4c
Eliezer Perez <eliezersierraperez@hotmail.com>
2024-07-29 04:35:33 +0200

    Widgets page d'accueil

--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -7,7 +7,12 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
     // Chargement du /css/widgets/image-titre-widget.css pour notre widget image titre
     wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/widgets/image-titre-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/image-titre-widget.css'));
+     // Chargement du /css/widgets/bloc-lien-image-widget.css pour notre widget bloc lien image
+     wp_enqueue_style('bloc-lien-image-widget', get_stylesheet_directory_uri() . '/css/widgets/bloc-lien-image-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/bloc-lien-image-widget.css'));
+   // Chargement du /css/shortcodes/banniere-titre.css pour notre shortcode banniere titre
+   wp_enqueue_style('banniere-titre-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/banniere-titre.css', array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/banniere-titre.css'));
 }
+
 //Creation du theme enfant 
 
 /* CHARGEMENT DES WIDGETS */
@@ -15,12 +20,49 @@ function theme_enqueue_styles()
 
 // On créer une class Widget Image_Titre_Widget dans un fichier à part pour pas surcharger le functions.php
 require_once(__DIR__ . '/widgets/ImageTitreWidget.php');
+require_once(__DIR__ . '/widgets/BlocLienImageWidget.php');
 
 function register_widgets()
 {
     //On enregistre le widget avec la class Image_Titre_Widget
     register_widget('Image_Titre_Widget');
+    register_widget('Bloc_Lien_Image_Widget');
 }
 //On demander à wordpress de charger des widget selon la fonction register_widgets()
 add_action('widgets_init', 'register_widgets');
 
+
+
+/* SHORTCODE */
+
+
+// Je dis à wordpress que j'ajoute un shortcode 'banniere-titre'
+add_shortcode('banniere-titre', 'banniere_titre_func');
+// Je génère le html retourné par mon shortcode
+function banniere_titre_func($atts)
+{
+    //Je récupère les attributs mis sur le shortcode
+    $atts = shortcode_atts(array(
+        'src' => '',
+        'titre' => 'Titre'
+    ), $atts, 'banniere-titre');
+
+    //Je commence à récupéré le flux d'information
+    ob_start();
+
+    if ($atts['src'] != "") {
+        ?>
+
+        <div class="banniere-titre" style="background-image: url(<?= $atts['src'] ?>)">
+            <h2 class="titre"><?= $atts['titre'] ?></h2>
+        </div>
+
+        <?php
+    }
+
+    //J'arrête de récupérer le flux d'information et le stock dans la fonction $output
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+}
---
+++ wp-content/themes/bricotips/templates/template-a-propos.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Template a propos
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+	Nous sommes dans la page a propos
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
